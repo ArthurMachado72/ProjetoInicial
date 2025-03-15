5c2dcb3bff31c1c09a7fbf24cccc7538c402019f
“ArthurMachado72” <luck10755@gmail.com>
2025-03-15 18:24:21 -0300

    Implementação inicial da nova funcionalidade

--- src/model/conectaDAO.java
+++ src/model/conectaDAO.java
@@ -1,33 +1,29 @@
+package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
 
-
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author Adm
- */
 public class conectaDAO {
-    
-    public Connection connectDB(){
+
+    public Connection connectDB() {
         Connection conn = null;
-        
+
         try {
-        
-            conn = DriverManager.getConnection("jdbc:mysql://localhost/uc11?user=root&password=");
-            
-        } catch (SQLException erro){
-            JOptionPane.showMessageDialog(null, "Erro ConectaDAO" + erro.getMessage());
+
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/uc11?useSSL=false&allowPublicKeyRetrieval=true",
+                    "luck",
+                    "0397154#_!54179330_#!WSX_yhn#!"
+            );
+
+            JOptionPane.showMessageDialog(null, "Conexão realizada com sucesso!");
+
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "Erro ao conectar no banco de dados: " + erro.getMessage());
         }
+
         return conn;
     }
-    
 }
